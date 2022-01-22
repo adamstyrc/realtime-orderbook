33c9b83bf11671366c9ed8ef849506e4ed7a14f0
astyrc <styrc.adam@gmail.com>
2022-01-22 17:04:10 +0100

    Prepare navigation routes.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+      },
     );
   }
 }
