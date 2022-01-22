6a9a88eeb296dee6e501d5bede97cb26fee2233d
astyrc <styrc.adam@gmail.com>
2022-01-22 17:28:55 +0100

    Fix warning

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
